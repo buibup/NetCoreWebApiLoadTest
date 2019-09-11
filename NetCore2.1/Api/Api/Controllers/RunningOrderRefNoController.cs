714d2c35567b6fb0cbf276794b0b078e24b48771
Mongkolm@hatari.net <mongkolm@hatari.net>
2019-09-11 11:26:20 +0700

    add netcore 2.1

---
+++ NetCore2.1/Api/Api/Controllers/RunningOrderRefNoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RunningOrderRefNoController : ControllerBase
+    {
+        public IRunningOrderRefNoRepository RunningOrderRefNoRepo { get; }
+        public RunningOrderRefNoController(IRunningOrderRefNoRepository runningOrderRefNoRepo)
+        {
+            RunningOrderRefNoRepo = runningOrderRefNoRepo;
+        }
+
+        [HttpGet]
+        public async Task<string> GetGetRunningOrderRefNo()
+        {
+            return await RunningOrderRefNoRepo.GetRunningOrderRefNo();
+        }
+    }
+}
